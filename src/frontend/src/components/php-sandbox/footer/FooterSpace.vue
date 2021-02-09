cbb67bba91f79ae19ee71c1a4784c89ec98d2ce7
Aleksey Leshchuk <leshchuk_ak@drom.ru>
2021-02-09 11:12:00 +1000

    add logos png and add buymecoffee button

--- src/frontend/src/components/php-sandbox/footer/FooterSpace.vue
+++ src/frontend/src/components/php-sandbox/footer/FooterSpace.vue
@@ -1,24 +1,34 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        Created with:
-      </p>
-      <div class="support-logos">
-        <a href="https://bulma.io/">
-          <img src="https://bulma.io/images/bulma-logo.png" alt="bulma">
-        </a>
-        <a href="https://microsoft.github.io/monaco-editor/">
-          <img src="https://opensource.microsoft.com/assets/images/Microsoft_logo.svg" alt="monaco-editor">
-        </a>
-        <a href="https://github.com/GoogleChromeLabs/dark-mode-toggle">
-          <img src="https://avatars.githubusercontent.com/u/31970254?s=200&v=4" alt="google-chrome-labs">
-        </a>
-        <a href="https://www.php.net/">
-          <img src="https://www.php.net/images/logos/php-logo.svg" alt="php-net">
+    <div class="columns">
+      <div class="column">
+        <div class="content has-text-centered">
+          <p>
+            Created with:
+          </p>
+          <div class="support-logos">
+            <a href="https://bulma.io/">
+              <img src="https://bulma.io/images/bulma-logo.png" alt="bulma">
+            </a>
+            <a href="https://microsoft.github.io/monaco-editor/">
+              <img src="https://opensource.microsoft.com/assets/images/Microsoft_logo.svg" alt="monaco-editor">
+            </a>
+            <a href="https://github.com/GoogleChromeLabs/dark-mode-toggle">
+              <img src="https://avatars.githubusercontent.com/u/31970254?s=200&v=4" alt="google-chrome-labs">
+            </a>
+            <a href="https://www.php.net/">
+              <img src="https://www.php.net/images/logos/php-logo.svg" alt="php-net">
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <a href="https://www.buymeacoffee.com/codetryclub">
+          <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=codetryclub&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff">
         </a>
       </div>
     </div>
+
   </footer>
 </template>
 
