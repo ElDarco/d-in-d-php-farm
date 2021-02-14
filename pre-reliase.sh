50accbf8ed4989ab9a6fbfd95e94101f98f87e29
Aleksey Leshchuk <leshchuk_ak@drom.ru>
2021-02-14 16:46:59 +1000

    fix delpoy

--- pre-reliase.sh
+++ pre-reliase.sh
@@ -4,7 +4,7 @@
 #./pre-reliase.sh token version
 
 echo "$1" | docker login --username eldarco --password-stdin
-docker build -t eldarco/code-try:front-"$2" -f docker/nodejs-builder/Dockerfile .
+docker build --compress -t eldarco/code-try:front-"$2" -f docker/nodejs-builder/Dockerfile .
 docker tag eldarco/code-try:front-"$2" eldarco/code-try:front-latest
 docker push eldarco/code-try:front-"$2"
 docker push eldarco/code-try:front-latest
