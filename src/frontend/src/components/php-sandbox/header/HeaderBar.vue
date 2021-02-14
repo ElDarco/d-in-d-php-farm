57e57354de8bfc66fcb470291af638a2ae52e0c4
Aleksey Leshchuk <leshchuk_ak@drom.ru>
2021-02-14 10:06:28 +1000

    add favicon

--- src/frontend/src/components/php-sandbox/header/HeaderBar.vue
+++ src/frontend/src/components/php-sandbox/header/HeaderBar.vue
@@ -2,7 +2,7 @@
   <div class="header-bar">
     <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
+        <a class="navbar-item" href="https://codetry.club">
           <img src="../../../assets/logo.svg">
         </a>
 
