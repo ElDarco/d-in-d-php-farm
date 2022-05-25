ad3660ef2ac806c6685c6ab0aab016ba7cd28816
Roman Dmitrienko <dmitrienko@drom.ru>
2022-05-25 17:35:17 +1000

    [fix] Наладить проксирование multipart/form-data

--- src/backend-mock-server/src/Middleware/NSpace/UseCase/CreateMockResponse.php
+++ src/backend-mock-server/src/Middleware/NSpace/UseCase/CreateMockResponse.php
@@ -2,7 +2,7 @@
 
 namespace Middleware\NSpace\UseCase;
 
-use Core\Mongo\SettingsCollectionProxy;
+use Core\Middleware\RequestDecoratorInterface;
 use DTO\NRequest;
 use DTO\NSettings;
 use DTO\NSpace;
@@ -14,6 +14,9 @@ use Laminas\Diactoros\Response;
 use Laminas\Diactoros\Response\EmptyResponse;
 use Middleware\InvokableMiddleware;
 use Psr\Http\Message\ResponseInterface;
+use function is_array;
+use function is_string;
+use function str_contains;
 
 class CreateMockResponse extends InvokableMiddleware
 {
@@ -48,7 +51,7 @@ class CreateMockResponse extends InvokableMiddleware
             $request = $request->withUri($uri);
 
             $client = new Client(['http_errors' => false]);
-            $response = $client->send($request);
+            $response = $client->send($request, $this->prepareOptions($request));
 // TODO Нет заголовков на http://localhost:18081/mock-server/n/d73039c5-647a-447e-be83-9ae917b1574b/123 https://google.com
 // TODO Ошибка ответа http://localhost:18081/mock-server/n/d73039c5-647a-447e-be83-9ae917b1574b/search?q=123 https://google.com
             $nProxyResponse = DtoFactory::createNProxyResponse($response->getBody()->getContents(), $response->getStatusCode(), $response->getHeaders());
@@ -61,5 +64,37 @@ class CreateMockResponse extends InvokableMiddleware
         }
 
         $this->getRequest()->withAttribute(ResponseInterface::class, $response);
+    }
+
+    private function prepareOptions(RequestDecoratorInterface $request): array
+    {
+        $options = [];
+
+        if (str_contains($request->getHeader('content-type')[0] ?? '', 'multipart/form-data')) {
+            $request->withoutHeader('content-type');
+            $options[RequestOptions::MULTIPART] = $this->transformMultipart($request->getParsedBody());
+        }
+
+        return $options;
+    }
+
+    private function transformMultipart(object|array|null $parsedBody): array
+    {
+        $result = [];
+
+        if (is_array($parsedBody)) {
+            // TODO: Добавить поддержку файлов 'contents' => Psr7\Utils::tryFopen('/path/to/file', 'r'),
+            // https://docs.guzzlephp.org/en/stable/request-options.html#multipart
+            foreach ($parsedBody as $fieldName => $fieldValue) {
+                if (is_string($fieldValue)) {
+                    $result[] = [
+                        'name' => $fieldName,
+                        'contents' => $fieldValue,
+                    ];
+                }
+            }
+        }
+
+        return $result;
     }
 }
